7f894ba6bf0f449a9fe442d22c2052650c140f0e
Muhammad Badrul <badrulalomtawsyat@gmail.com>
2022-11-26 03:58:53 +0000

    Badrul

--- 26nov/pattern.sh
+++ 26nov/pattern.sh
@@ -1,9 +1,7 @@
 #! /usr/bin/sh
 
-#triangle pattern
 for i in 1 2 3 4 5
 do
-#loop for leading space
     for j in 5 4 3 2 1
     do
         if [ $j -ge $i ]
@@ -11,7 +9,6 @@ do
             echo -n " "
         fi
     done
-#loop for printing 
     for k in 1 2 3 4 5
     do
         if [ $k -le $i ]
